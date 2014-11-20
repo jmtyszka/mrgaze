1db9a34da9d08dc51ec5995ae190981a036321b7
Mike Tyszka <mike.tyszka@gmail.com>
2014-11-20 08:29:16 -0800

    RT-MRGAZE v0.1.1

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup ( name = 'mrgaze',
-        version = '0.5.2',
-        description = 'Offline video pupilometry and gaze tracking',
+        version = '0.1.1',
+        description = 'Real-time pupilometry module'
         author = ['Mike Tyszka'],
         author_email = ['jmt@caltech.edu'],
-        url = ['https://github.com/jmtyszka/geetee'],
+        url = ['https://github.com/jmtyszka/mrgaze'],
         license = 'LICENSE.txt',
         packages = find_packages(),
         scripts = ['mrgaze_batch.py', 'mrgaze_single.py'],
