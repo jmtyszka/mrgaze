f0a5b5b16c166678ac012c496583c626e5e32869
Dan Weflen <danweflen@gmail.com>
2016-02-08 17:27:54 -0700

    Added parentheses to calls to print. This improves python3 compatibility without affecting mrgaze in python2.

--- mrgaze/utils.py
+++ mrgaze/utils.py
@@ -62,8 +62,8 @@ def _package_root():
             root = os.path.realpath(root)
         return os.path.dirname (os.path.abspath(root))
     except:
-        print "I'm sorry, but something is wrong."
-        print "There is no __file__ variable. Please contact the author."
+        print ("I'm sorry, but something is wrong.")
+        print ("There is no __file__ variable. Please contact the author.")
         sys.exit(1)
 
 
